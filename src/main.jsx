ab7e4bb3f0097d6c8a1edaef0977f178beb0c18a
Manuel Agapito Samaniego Lao <80022993+Mansama18-code@users.noreply.github.com>
2023-11-30 17:01:19 -0300

    Add files via upload
    Tercer carga

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import Menus from './menus.jsx';
-import Slider_header from './slider_header.jsx';
+import Sesion from './sesion.jsx';
 import './index.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Menus />
-    <Slider_header />
-    <App />
+    <Sesion />
   
   </React.StrictMode>,
 )
---
+++ src/sesion.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import App from './App.jsx';
+import Menus from './menus.jsx';
+import Slider_header from './slider_header.jsx';
+
+const sesion = () => {
+  return (
+    <div>
+        <Menus />
+        <Slider_header />
+        <App />
+
+
+    </div>
+  )
+}
+
+export default sesion
