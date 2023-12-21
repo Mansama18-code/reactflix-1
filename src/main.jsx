0b858e5ca22f55235faa98a366d9dc584d518ffd
Manuel Agapito Samaniego Lao <80022993+Mansama18-code@users.noreply.github.com>
2023-12-21 06:24:39 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Contador from './Contador.jsx';
+
 import Sesion from './sesion.jsx';
-import Estados from './Estados.jsx';
+
 import Iniciosesion from './iniciosesion.jsx';
 import './index.css';
 
